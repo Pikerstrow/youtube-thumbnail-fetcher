bfb85e9f99ac63fc10fb75d09c064e4ca2201208
pikerstrow <sasha@adscendmedia.com>
2022-01-26 10:07:34 +0200

    Button title changed

--- resources/lang/en/views.php
+++ resources/lang/en/views.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'h1' => 'Get YouTube Video Preview Image',
-    'description' => 'The simplest and the most fast way to retrieve preview image for YouTube videos. Just paste URL of the video in the input below and press the button. As a result, you\'ll get links to thumbnail in all available resolutions and link to the zip archive with files.',
+    'description' => 'The simplest and the most fast way to get thumbnail image for any YouTube video. Just paste URL of the video in the input below and press the button. As a result, you\'ll get links to thumbnail in all available resolutions and link to the zip archive with files.',
     'note' => 'Note: ',
     'note_txt' => 'supported URL format: https://www.youtube.com/watch?v=XvtrI5hOIij7',
     'result_title' => 'Max resolution thumbnail image for YouTube video by provided URL is',
