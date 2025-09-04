197b91cf7f1fb34fd8a294f79104a82c408b79f2
pikerstrow <sasha@adscendmedia.com>
2025-09-04 20:31:05 +0300

    mobile css fixedd

--- resources/lang/en/views.php
+++ resources/lang/en/views.php
@@ -5,7 +5,7 @@ return [
         'h2_additional' => 'Download Video Thumbnails from YouTube in Seconds!',
         'description' => 'The simplest and the most fast way to get (download) thumbnail image for any YouTube video. Just paste URL of the video in the input below and press the button. As a result, you\'ll get links to thumbnail in all available resolutions and link to the zip archive with files.',
         'note' => 'Note: ',
-        'note_txt' => 'supported URL format: https://www.youtube.com/watch?v=XvtrI5hOIij7',
+        'note_txt' => 'supported URL format: https://www.youtube.com/watch?v=XvtrI5hOIij7 OR https://www.youtube.com/shorts/TS-e8ewewe4',
         'result_title' => 'Max resolution thumbnail image for YouTube video by provided URL is',
         'links_note' => ' You can download this image by right click of the mouse or use links below. Also you can download zip archive with images of all available resolutions.',
         'h3_links' => 'Links for thumbnails with different resolutions',
@@ -38,6 +38,6 @@ return [
         'description' => 'The simplest way to download and save any YouTube video on your device. Just paste URL of the video in the input below and press the button. As a result you\'ll get all information about the video with downloading links!',
         'fetch' => 'Start Downloading',
         'note' => 'Note: ',
-        'note_txt' => 'supported URL format: https://www.youtube.com/watch?v=XvtrI5hOIij7',
+        'note_txt' => 'supported URL format: https://www.youtube.com/watch?v=XvtrI5hOIij7 OR https://www.youtube.com/shorts/TS-e8ewewe4',
     ]
 ];
